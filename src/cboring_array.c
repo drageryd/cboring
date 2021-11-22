599c9ce3892206e51e7181d6456cb12a99ca8c9e
drageryd <4681360+drageryd@users.noreply.github.com>
2021-11-22 19:22:27 +0100

    Get length and item at index in array

--- src/cboring_array.c
+++ src/cboring_array.c
@@ -7,3 +7,42 @@ bool cbor_is_array(const uint8_t *buffer, size_t len) {
     cbor_get_argument(buffer, len, &argument);
     return MAJOR_TYPE(buffer) == CBOR_ARRAY;
 }
+
+/* Get the number of items in an array */
+size_t cbor_array_length(const uint8_t *buffer, size_t len) {
+    /* The head and argument occupy some space, find where
+     * the first map item starts */
+    uint64_t argument;
+    size_t array_offset = cbor_get_argument(buffer, len, &argument);
+
+    /* A definite array holds the number of items in its argument */
+    if (ADDITIONAL_INFO(buffer) != CBOR_INDEFINITE) {
+        return argument;
+    }
+    /* Indefinite arrays need to be iterated to know the length */
+    size_t array_length = 0;
+    while (*(buffer + array_offset) != CBOR_STOP_CODE) {
+        array_length++;
+        /* Get item size */
+        array_offset += cbor_item_size(buffer + array_offset, len - array_offset);
+    }
+    return array_length;
+}
+
+/* Get item at index from array */
+size_t cbor_array_index(const uint8_t *buffer, size_t len, size_t index) {
+    uint64_t argument;
+    size_t array_offset = cbor_get_argument(buffer, len, &argument);
+    const size_t array_length = cbor_array_length(buffer, len);
+    if (index >= array_length) {
+        /* Index out of range */
+        return len;
+    }
+    /* Go past "index" number of items to get to desired item */
+    for (size_t i = 0; i < index; i++) {
+        /* Move to next key by incrementing offset past items */
+        array_offset += cbor_item_size(buffer + array_offset, len - array_offset);
+    }
+    /* End of loop reached array_offset is now at array index */
+    return array_offset;
+}
--- src/cboring_array.h
+++ src/cboring_array.h
@@ -6,5 +6,7 @@
 #include <stdbool.h>
 
 bool cbor_is_array(const uint8_t *buffer, size_t len);
+size_t cbor_array_length(const uint8_t *buffer, size_t len);
+size_t cbor_array_index(const uint8_t *buffer, size_t len, size_t index);
 
 #endif
