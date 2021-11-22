74a54940f22989b7258271b52459a063ea0577bd
drageryd <4681360+drageryd@users.noreply.github.com>
2021-11-22 19:20:07 +0100

    Remove magic values in get argument

--- src/cboring_internal.c
+++ src/cboring_internal.c
@@ -21,25 +21,24 @@ size_t cbor_getn(const uint8_t *buffer, size_t n, uint64_t *data) {
  * This can be embedded in the first byte or
  * in the following 1, 2, 4 or 8 bytes 
  * Return value: length of head */
-/* TODO: Magic values */
 size_t cbor_get_argument(const uint8_t *buffer, size_t len, uint64_t *argument) {
     /* The initial byte has major type in upper 3 bits and
      * additional information in lower 5 bits */
     /* At least one byte in buffer */
     assert(len > 0);
-    uint8_t addinf = *buffer & 0x1f;
-    if (addinf < 24) {
+    uint8_t addinf = ADDITIONAL_INFO(buffer);
+    if (addinf < CBOR_FOLLOWING_1) {
         /* Argument is embedded in initial byte */
         *argument = (uint64_t)addinf;
         return 1;
-    } else if (addinf < 28) {
+    } else if (addinf <= CBOR_FOLLOWING_8) {
         /* Additional info 24, 25, 26 and 27 loads
          * 1, 2, 4 and 8 bytes respectively */
-        size_t n = 1 << (addinf - 24);
+        size_t n = 1 << (addinf - CBOR_FOLLOWING_1);
         /* At least n bytes left after initial byte */
         assert(len > n);
         return 1 + cbor_getn(buffer + 1, n, argument);
-    } else if (addinf == 31) {
+    } else if (addinf == CBOR_INDEFINITE) {
         /* Indefinite, set to max */
         *argument = UINT64_MAX;
         return 1;
--- src/cboring_internal.h
+++ src/cboring_internal.h
@@ -15,9 +15,19 @@ enum {
     CBOR_FLOAT_AND_SIMPLE,
 };
 
+enum {
+    CBOR_FOLLOWING_1 = 24,
+    CBOR_FOLLOWING_2,
+    CBOR_FOLLOWING_4,
+    CBOR_FOLLOWING_8,
+    CBOR_INDEFINITE = 31,
+    CBOR_STOP_CODE = 0xff,
+};
+
 /* Major type is stored in the upper 5 bits of the head
  * Takes any CBOR buffer as input and dereferences the first byte */
 #define MAJOR_TYPE(x) (*x >> 5)
+#define ADDITIONAL_INFO(x) (*x & 0x1f)
 
 size_t cbor_getn(const uint8_t *buffer, size_t n, uint64_t *data);
 size_t cbor_get_argument(const uint8_t *buffer, size_t len, uint64_t *argument);
